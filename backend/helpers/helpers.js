a30a4563acf2f1eb8ec404e33fd6f7568caca309
AminesCodes <aminebensalem@pursuit.org>
2020-07-28 23:35:02 -0400

    hashPassword and comparePasswords functions using bcrypt

--- backend/helpers/helpers.js
+++ backend/helpers/helpers.js
@@ -1,8 +1,21 @@
+const bcrypt = require('bcrypt');
+
+const hashPassword = async (password) => {
+    const salt = await bcrypt.genSalt(12);
+    return await bcrypt.hash(password, salt);
+}
+
+const comparePasswords = async (password, passwordDigest) => {
+    return await bcrypt.compare(password, passwordDigest); // TRUE if a match, FALSE if not
+}
+
 const formatStr = str => {
     return str.toLowerCase().replace(/[^a-z0-9]/g, '')
 }
 
 
 module.exports = {
+    hashPassword,
+    comparePasswords,
     formatStr,
 }
